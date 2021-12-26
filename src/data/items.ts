c302920da547741cedbc3f670de59a3e58dc9061
Sat Naing <satnaing@Sats-MacBook-Air.local>
2021-12-26 23:15:48 +0630

    :seedling: add local seed file - items.ts

---
+++ src/data/items.ts
@@ -0,0 +1,120 @@
+export const items = [
+  {
+    id: "1",
+    name: "Long Sleeve Checked Shirt",
+    price: "60",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FLong%20SleeveCheckedShirt-img2.jpg?alt=media&token=3a9c3759-d7c5-484f-a6b0-288b205c06f5",
+    image2: "image2",
+    categoryId: "1",
+  },
+  {
+    id: "2",
+    name: "Zipped Joggers",
+    price: "199.99",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FZippedJoggers-img2.jpg?alt=media&token=316b1d0a-7975-4f74-bc9f-c3020536e166",
+    image2: "image2",
+    categoryId: "1",
+  },
+  {
+    id: "3",
+    name: "Pleated Shirt Dress",
+    price: "68.84",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FPleatedShirtDress-img1.jpg?alt=media&token=fbbebda0-ea6c-493d-b4f8-7bfea245758e",
+    image2: "image2",
+    categoryId: "2",
+  },
+  {
+    id: "4",
+    name: "Pleated Midi Skirt",
+    price: "59.39",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FPleatedMidiSkirt-img1.jpg?alt=media&token=a3ad8032-f706-4ad3-87db-0a6d971aafed",
+    image2: "image2",
+    categoryId: "2",
+  },
+  {
+    id: "5",
+    name: "Floral Midi Skirt",
+    price: "89.99",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FFloralMidiSkirt-img2.jpg?alt=media&token=986733fa-724c-4236-85cf-bd6154366f7c",
+    image2: "image2",
+    categoryId: "2",
+  },
+];
+
+export const newItems = [
+  {
+    id: "1",
+    name: "Short Straight Coat",
+    price: "89.92",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FShortStraightCoat-img1.jpg?alt=media&token=fd3b3da3-bae3-4ec7-a02f-790dfa13712e",
+    image2:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FShortStraightCoat-img2.jpg?alt=media&token=ff26ad6a-5249-4939-a7df-fffcb2d3efbb",
+    categoryId: "1",
+  },
+  {
+    id: "2",
+    name: "Pleated Shirt Dress",
+    price: "99.99",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FPleatedShirtDress-img1.jpg?alt=media&token=fbbebda0-ea6c-493d-b4f8-7bfea245758e",
+    image2:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FPleatedShirtDress-img2.jpg.webp?alt=media&token=e672bd8a-723c-4fa2-98f7-414b0ab5404a",
+    categoryId: "1",
+  },
+  {
+    id: "3",
+    name: "Joggers With Mesh Detail",
+    price: "65.88",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FJoggersWithMeshDetail-img1.jpg?alt=media&token=0199a663-a84f-45d2-ba51-5f1206c479eb",
+    image2:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FJoggersWithMeshDetail-img2.jpg?alt=media&token=c144e821-f283-4dfd-a505-96bbaf829415",
+    categoryId: "2",
+  },
+  {
+    id: "4",
+    name: "Gray Flare Jeans",
+    price: "48.39",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FGrayFlareJeans-img1.jpg?alt=media&token=5d20f43e-96a5-43e3-a72c-6d330da23190",
+    image2:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FGrayFlareJeans-img2.jpg?alt=media&token=680135f7-efbf-4840-af32-4bda1f021279",
+    categoryId: "2",
+  },
+  {
+    id: "5",
+    name: "Quilted Bomber Jacket",
+    price: "89.99",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora magnam debitis dolor veniam consequatur numquam perferendis cum, ipsam aliquid ipsa?",
+    image1:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FQuiltedBomberJacket-img1.jpg?alt=media&token=564fd7de-bfaa-4efb-bbd3-35b26cb974f9",
+    image2:
+      "https://firebasestorage.googleapis.com/v0/b/ecommerce-sn.appspot.com/o/items%2FQuiltedBomberJacket-img2.jpg?alt=media&token=0a3d2b11-e47e-4eeb-a4e0-8641e19f9007",
+    categoryId: "2",
+  },
+];
