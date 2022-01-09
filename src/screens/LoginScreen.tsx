7bb74a09e708c0c1f2b4ad50ddaa3e49db2ad82a
Sat Naing <satnaing@Sats-MacBook-Air.local>
2022-01-09 22:42:05 +0630

    :sparkles: implement Login and Register screens

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  AspectRatio,
+  Box,
+  Button,
+  Center,
+  Divider,
+  FlatList,
+  FormControl,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  Pressable,
+  Radio,
+  Text,
+  VStack,
+} from "native-base";
+import { items, newItems } from "../data/items";
+import { TouchableOpacity } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <Box flex={1} bg="light.200">
+      <Center bg="light.500" height="25%">
+        <Image
+          style={{ width: "30%", height: "40%" }}
+          size="xl"
+          resizeMode="cover"
+          source={require("../assets/logo.png")}
+          alt="Haru Fashion"
+        />
+      </Center>
+      <Heading my={2} alignSelf="center">
+        Login
+      </Heading>
+      <Box mx={4}>
+        <Text>Email</Text>
+        <Input
+          _focus={{ borderColor: "light.300" }}
+          mb={4}
+          variant="outline"
+          placeholder="Enter your email"
+        />
+
+        <Text>Password</Text>
+        <Input
+          _focus={{ borderColor: "light.300" }}
+          mb={4}
+          variant="outline"
+          placeholder="Enter your password"
+        />
+
+        <TouchableOpacity>
+          <Text color="light.300">Forgot Password?</Text>
+        </TouchableOpacity>
+
+        <Button
+          background="light.500"
+          _pressed={{ bg: "light.400" }}
+          _text={{ color: "light.100", fontSize: "md" }}
+          height="12"
+          width="60%"
+          borderColor="light.500"
+          alignSelf="center"
+          my={6}
+        >
+          Login
+        </Button>
+        <HStack alignSelf="center">
+          <Text color="light.300">Not a member? </Text>
+          <TouchableOpacity>
+            <Text>Register Now!</Text>
+          </TouchableOpacity>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginScreen;
---
+++ src/screens/RegisterScreen.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  Text,
+} from "native-base";
+import { items, newItems } from "../data/items";
+import { TouchableOpacity } from "react-native";
+
+const RegisterScreen = () => {
+  return (
+    <Box flex={1} bg="light.200">
+      <Center bg="light.500" height="25%">
+        <Image
+          style={{ width: "30%", height: "40%" }}
+          size="xl"
+          resizeMode="cover"
+          source={require("../assets/logo.png")}
+          alt="Haru Fashion"
+        />
+      </Center>
+      <Heading my={2} alignSelf="center">
+        Register
+      </Heading>
+      <Box mx={4}>
+        <Text>Email</Text>
+        <Input
+          _focus={{ borderColor: "light.300" }}
+          mb={4}
+          variant="outline"
+          placeholder="Enter your email"
+        />
+
+        <Text>Password</Text>
+        <Input
+          _focus={{ borderColor: "light.300" }}
+          mb={4}
+          variant="outline"
+          placeholder="Enter your password"
+        />
+
+        <Button
+          background="light.500"
+          _pressed={{ bg: "light.400" }}
+          _text={{ color: "light.100", fontSize: "md" }}
+          height="12"
+          width="60%"
+          borderColor="light.500"
+          alignSelf="center"
+          my={6}
+        >
+          Register
+        </Button>
+        <HStack alignSelf="center">
+          <Text color="light.300">Already a member? </Text>
+          <TouchableOpacity>
+            <Text>Login Now!</Text>
+          </TouchableOpacity>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default RegisterScreen;
